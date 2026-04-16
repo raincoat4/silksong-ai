3617778c873aa4063f05efeec1c04200badb8569
ltr1 <ltr1@sfu.ca>
2026-04-16 16:47:35 -0700

    file restructure and added get_movement folder

---
+++ vision/get_movement/create_get_movement_dataset.py
@@ -0,0 +1,32 @@
+import mss
+import cv2
+import numpy as np
+import time
+import os
+import re
+from vision.vision_service import get_window_coords
+
+# Ensure folder exists
+folder = "vision/training_data/get_movement/training_data/photos"
+os.makedirs(folder, exist_ok=True)
+
+files = os.listdir(folder)
+indices = []
+if len(files) != 0:
+    match = re.search(r"frame(\d+)", files[len(files)- 1])
+    i = int(match.group(1))
+else:
+    i = 0   
+
+print(i)
+
+# with mss.mss() as sct:
+#     while True:
+#         time.sleep(5)
+#         window = get_window_coords()
+#         screenshot = np.array(sct.grab(window))
+#         img= cv2.cvtColor(screenshot, cv2.COLOR_BGRA2BGR)
+#         filename = os.path.join(folder, f"frame{i}.png")
+#         cv2.imwrite(filename, img)
+#         print(f"Saved {i}")
+#         i += 1
